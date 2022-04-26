7ef71f709c7113ffb641561b4bf4dc5d4d742ae9
Viktor Tagil <viktor.tagil@gmail.com>
2022-04-26 13:44:20 -0700

    app set up

---
+++ random-number/random-number/random-number/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Clicker from './Clicker'
+
+function App() {
+  return (
+    <div className="App">
+      <Clicker />
+    </div>
+  );
+}
+
+export default App;
---
+++ random-number/random-number/random-number/src/Clicker.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react'
+
+class Clicker extends Component {
+  render() {
+    return <h1>Clicker</h1>
+  }
+}
+
+export default Clicker;
