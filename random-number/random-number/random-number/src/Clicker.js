289cf4946aad9afaf6467abc6aa057d8a3e4e502
Viktor Tagil <viktor.tagil@gmail.com>
2022-04-26 14:42:29 -0700

    conditional logic added

--- random-number/random-number/random-number/src/Clicker.js
+++ random-number/random-number/random-number/src/Clicker.js
@@ -1,8 +1,28 @@
 import React, {Component} from 'react'
 
 class Clicker extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { num: 1 };
+    this.genRandom = this.genRandom.bind(this)
+  }
+  genRandom(){
+    // pick random number
+    let rand = Math.floor(Math.random() * 10) + 1
+    // Update state with new random number
+    this.setState({ num: rand });
+    //bind
+  }
   render() {
-    return <h1>Clicker</h1>
+    return (
+      <div>
+        <h1>Number is: {this.state.num}</h1>
+        {this.state.num === 7
+          ? <h2>You winn</h2>
+          : <button onClick={this.genRandom}>Random Number</button>
+        }
+      </div>
+    );
   }
 }
 
