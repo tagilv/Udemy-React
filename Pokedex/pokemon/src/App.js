0ab3d6c479de657d397e3e7085f4a8ae725e2c46
Viktor Tagil <viktor.tagil@gmail.com>
2022-04-25 17:26:29 -0700

    pokemon added

--- Pokedex/pokemon/src/App.js
+++ Pokedex/pokemon/src/App.js
@@ -1,9 +1,17 @@
 import logo from './logo.svg';
+import Pokecard from './Pokecard'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Pokecard
+      id={4}
+      name="Charmander"
+      type="fire"
+      exp={62}
+
+      />
     </div>
   );
 }
