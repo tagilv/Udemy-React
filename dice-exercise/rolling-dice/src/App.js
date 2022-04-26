91bd0ba83ae649198822e22b1b14f229ab816f94
Viktor Tagil <viktor.tagil@gmail.com>
2022-04-26 16:48:41 -0700

    dice component added and css added

---
+++ dice-exercise/rolling-dice/src/App.js
@@ -0,0 +1,15 @@
+import Die from './Die';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Die face='three'/>
+      <Die face='four'/>
+      <Die face='five'/>
+      <Die face='six'/>
+    </div>
+  );
+}
+
+export default App;
