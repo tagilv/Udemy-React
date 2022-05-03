5859b4ead4562a98d6c78612123c8d91b8ab69ae
Viktor Tagil <viktor.tagil@gmail.com>
2022-05-03 17:07:49 +0200

    boxes rendering

--- colors/color-palette/src/App.js
+++ colors/color-palette/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
 import Box from './Box';
 import './App.css';
+import BoxContainer from './BoxContainer'
 
 function App() {
   return (
     <div className="App">
-     < Box />
+     < BoxContainer />
     </div>
   );
 }
