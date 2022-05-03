b9e8f3f470bf899f7c363f3058d5deabf1cafb9f
Viktor Tagil <viktor.tagil@gmail.com>
2022-05-03 12:10:22 +0200

    coin flip working

--- coin-flip/coin-flip/src/Coin.js
+++ coin-flip/coin-flip/src/Coin.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react'
+import './Coin.css'
 
 class Coin extends Component {
   render () {
     return (
-      <h1>Coin</h1>
+      <div className='Coin'>
+      <img src={this.props.info.imgSrc} alt={this.props.info.side}/>;
+      </div>
     )
   }
 }
--- coin-flip/coin-flip/src/CoinContainer.js
+++ coin-flip/coin-flip/src/CoinContainer.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import { choice } from './helpers';
+import Coin from './Coin'
 
 class CoinContainer extends Component {
   static defaultProps = {
     coins: [
-      {side: 'heads', imgSrc: 'https://tinyurl.com/react-coin-heads-jpg'},
-      {side: 'heads', imgSrc: 'https://tinyurl.com/react-coin-tails-jpg'}
+      {side: 'heads', imgSrc: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2e/US_One_Cent_Obv.png/1200px-US_One_Cent_Obv.png'},
+      {side: 'tails', imgSrc: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/2017-D_Roosevelt_dime_reverse_transparent.png/220px-2017-D_Roosevelt_dime_reverse_transparent.png'}
     ]
   }
   constructor(props) {
@@ -23,7 +24,9 @@ class CoinContainer extends Component {
     this.setState(st => {
       return {
         currCoin: newCoin,
-        nFlips: st.nFlips + 1
+        nFlips: st.nFlips + 1,
+        nHeads: st.nHeads + (newCoin.side === 'heads' ? 1 : 0),
+        nTails: st.nTails + (newCoin.side === 'tails' ? 1 : 0)
       };
     })
   }
@@ -34,7 +37,9 @@ class CoinContainer extends Component {
     return (
       <div className="CoinContainer">
         <h2>Lets flip a coin</h2>
-        <p>Out of {this.state.nFlips} flips, there have been {this.state.nHeads} heads and {this.state.nTails} tails </p>
+        <p>Out of {this.state.nFlips} flips, there have been {this.state.nHeads} heads and {this.state.nTails} tails
+        </p>
+        {this.state.currCoin && <Coin info={this.state.currCoin} />}
         <button onClick={this.handleClick}>Click me!</button>
       </div>
     )
