abac813908549802472a28830d98a899c954abc6
Viktor Tagil <viktor.tagil@gmail.com>
2022-06-23 22:42:53 +0200

    hangman game completed

--- hangman/react-hangman/src/Hangman.js
+++ hangman/react-hangman/src/Hangman.js
@@ -69,7 +69,12 @@ class Hangman extends Component {
   /** render: render game */
   render() {
     let gameOver = this.state.nWrong >= this.props.maxWrong
+    const isWinner = this.guessedWord().join("") === this.state.answer
     const altText = `${this.state.nWrong}/${this.props.maxWrong} guesses`;
+    let gameState = this.generateButtons();
+    if (isWinner) gameState = "You Win"
+    if (gameOver) gameState = "You Lose"
+
     return (
       <div className='Hangman'>
         <h1>Hangman</h1>
@@ -77,10 +82,9 @@ class Hangman extends Component {
         <p>Guessed Wrong: {this.state.nWrong} </p>
         <p className='Hangman-word'>{!gameOver ? this.guessedWord() : this.state.answer}</p>
         <p className='Hangman-btns'>
-          {!gameOver ? this.generateButtons()
-          : `You lose`}
+          {gameState}
         </p>
-        <button onClick={this.reset}>Restart</button>
+        <button id="reset" onClick={this.reset}>Restart</button>
       </div>
     );
   }
